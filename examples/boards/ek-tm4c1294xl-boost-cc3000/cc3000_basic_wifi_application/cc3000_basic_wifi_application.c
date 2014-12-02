593db275114ffdcb8bffb16598f0fb6b6df7fdbb
Luis Bill <elpot_15@hotmail.com>
2014-12-02 15:57:45 -0800

    correcting IR sensor distance with 5V power

--- examples/boards/ek-tm4c1294xl-boost-cc3000/cc3000_basic_wifi_application/cc3000_basic_wifi_application.c
+++ examples/boards/ek-tm4c1294xl-boost-cc3000/cc3000_basic_wifi_application/cc3000_basic_wifi_application.c
@@ -345,27 +345,18 @@ void ADC0_InitSWTriggerSeq3_Ch9(void){
 }
 
 unsigned long ADC0_InSeq3(void){  
-	/*
-	unsigned long result;
-  ADC0_PSSI_R = 0x0008;            // 1) initiate SS3
-  while((ADC0_RIS_R&0x08)==0){};   // 2) wait for conversion done
-  result = ADC0_SSFIFO3_R&0xFFF;   // 3) read result
-  ADC0_ISC_R = 0x0008;             // 4) acknowledge completion
-	*/
 	ADCIntClear(ADC0_BASE, 1);
 	ADCProcessorTrigger(ADC0_BASE, 1);
 	while(!ADCIntStatus(ADC0_BASE, 1, false))
 	{
 	}
-	ADCSequenceDataGet(ADC0_BASE, 1, ui32IRValues);
+	ADCSequenceDataGet(ADC0_BASE, 1, ui32IRValues);   // get data from FIFO
 	ui32LeftSensor = ui32IRValues[0];
 	ui32RightSensor = ui32IRValues[1];
-	//float res = 0.0;
-	// transform voltage to distance
-	//res = powf(8.0, -12.0)*powf(result,3.0) - powf(3.0, -8.0)*powf(result, 2.0) + powf(8.0, -5.0)*result - 0.0026;
 	// resutl is given in voltage, and the formula gives (1/cm)
 	// so we invert the result of the convertion to get (cm)
-	return (1.0/(powf(7.0, -5)*ui32LeftSensor*1.0 - 0.0022));
+	//return (1.0/(powf(7.0, -5)*ui32LeftSensor*1.0 - 0.0022));
+	return (1.0/((37.0/648000.0)*ui32LeftSensor*1.0 - (67.0/6480.0)));
 }
 
 
